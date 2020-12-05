822ef413276bd021f164e16b070aad26cf2d30d8
maxwhyso <75530584+maxwhyso@users.noreply.github.com>
2020-12-05 22:19:11 +0200

    Add files via upload

---
+++ 4laboratorna2zavdana.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+  int i,j;	
+  int a[3][3] = { 29, 0, -1, 3, 0, 13, -8, 0, 29  };
+  
+  
+          printf("%d %d %d\n", a[0][0], a[0][1], a[0][2]);
+            printf("%d %d %d\n", a[1][0], a[1][1], a[1][2]);
+              printf("%d %d %d\n", a[2][0], a[2][1], a[3][2]);
+  
+  for (i=0; i<3; i++)
+  
+  for (j=0; j<3; j++)
+  
+  
+{
+	
+	
+	if (a[i][j]==0);
+	
+	
+
+	printf ("\n %d",a[i][j]);
+	
+	
+}
+  
+  
+
+
+    
+	return 0;
+}
